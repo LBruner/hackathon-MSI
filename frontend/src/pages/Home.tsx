798e14aed206abf0124b3d6584f784717d119e85
LBruner <leandrobruner15@gmail.com>
2023-10-18 08:32:56 -0300

    add home screen

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import {useLocation} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
+import Card from "../components/UI/Card";
+import agendaImg from '../assets/images/register.png'
 
 const Home: React.FC = _ => {
-    const location = useLocation();
-    console.log(location.pathname)
+    const navigate = useNavigate();
+    const onGoToPath = (path: string) => {
+        navigate(path)
+    }
+
     return (
-        <div>
-            <h1>OI</h1>
+        <div className={'w-100 h-100 d-flex justify-content-center'}>
+            <Card title={'Chamada'} description={'Faça a chamada de seus funcionários'} img={agendaImg}
+                  fn={onGoToPath.bind(null, '/employeeRegister')} buttonTxt={'Fazer chamada'}/>
+            <Card title={'Funcionários'}
+                  description={'Gerenciar funcionários'} img={agendaImg}
+                  fn={onGoToPath.bind(null, '/employees')} buttonTxt={'Gerenciar'}/>
         </div>
     )
 }
