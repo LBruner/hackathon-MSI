2c94ba09d9c3801577d0cda44af3fee0ae2314fd
LBruner <leandrobruner15@gmail.com>
2023-10-18 08:31:18 -0300

    add construction controller

---
+++ backend/src/controllers/constructionsController.ts
@@ -0,0 +1,30 @@
+import query from "../models/db/dbConnection";
+import {RequestHandler} from "express";
+
+export interface Employee {
+    id: string,
+    name: string,
+    photo_uri: string,
+    position: string,
+}
+
+export const getAllBuldings: RequestHandler = async (req, res) => {
+    console.log("OIOI")
+    const results = await query(`SELECT *
+                                 FROM constructions` as any);
+    res.send(results).status(200);
+}
+
+export const addConstruction: RequestHandler = async (req, res) => {
+    const {name, photo_uri, start_date, employees_to_add} = req.body;
+    console.log(req.body)
+    const results = await query(`INSERT INTO constructions(name, photo_uri, start_date) VALUE ("${name}", "${photo_uri}", "${start_date}")` as any);
+    // @ts-ignore
+    const insertId = results.insertId;
+
+    for (const employee of employees_to_add) {
+        await query(`INSERT INTO functionariesConstructions (functionaryId, constructionId) VALUE ("${employee.id}", "${insertId}")` as any);
+    }
+
+    res.send(200);
+}
