25a518e025bbe6a7489eb6239054a04cd246cc59
LBruner <leandrobruner15@gmail.com>
2023-10-18 08:31:39 -0300

    add employee controller

---
+++ backend/src/controllers/employeeController.ts
@@ -0,0 +1,89 @@
+import query from "../models/db/dbConnection";
+import {RequestHandler} from "express";
+
+export const getAllEmployees: RequestHandler = async (req, res
+) => {
+    const employees = await query(`SELECT *
+                                   from functionaries` as any);
+    res.send(employees);
+}
+
+export const getEmployeesByConstruction: RequestHandler = async (req, res) => {
+    const {id} = req.params;
+    console.log(id)
+    const results = await query(`SELECT DISTINCT functionaries.id,
+                                                 functionaries.name,
+                                                 functionaries.position,
+                                                 functionaries.photo_uri
+                                 from functionaries
+                                          join construtora.functionariesConstructions
+                                               on functionaries.id = functionariesConstructions.functionaryId
+                                          join construtora.constructions c
+                                               on functionariesConstructions.constructionId = c.id
+                                 where constructionId = "${id}"
+    ` as any);
+    console.log(results)
+    res.send(results)
+}
+
+export const getEmployeesByPeriod: RequestHandler = async (req, res) => {
+    const {id, period} = req.params;
+
+    // @ts-ignore
+    const markedEmployees: Employees[] = await query(`SELECT DISTINCT functionaries.id,
+                                                                      functionaries.name,
+                                                                      functionaries.position,
+                                                                      functionaries.photo_uri
+                                                      from functionaries
+                                                               join construtora.functionariesConstructions
+                                                                    on functionaries.id = functionariesConstructions.functionaryId
+                                                               join construtora.calls AS c on functionaries.id = c.employeeId
+                                                               join construtora.constructions c2
+                                                                    on functionariesConstructions.constructionId = c2.id
+                                                      WHERE c.periodo = '${period}';
+    ` as any);
+
+    const unmarkedEmployees = await query(`SELECT functionaries.id,
+                                                  functionaries.name,
+                                                  functionaries.position,
+                                                  functionaries.photo_uri
+                                           FROM functionaries
+                                                    JOIN construtora.functionariesConstructions
+                                                         ON functionaries.id = functionariesConstructions.functionaryId
+                                                    JOIN construtora.constructions c2
+                                                         ON functionariesConstructions.constructionId = c2.id
+                                                    LEFT JOIN construtora.calls AS c
+                                                              ON functionaries.id = c.employeeId
+                                                                  AND c.periodo = "${period}"
+                                           WHERE c2.id = ${id}
+                                             AND c.employeeId IS NULL;` as any);
+
+    res.send({
+        log: {
+            markedEmployees,
+            unmarkedEmployees
+        }
+    });
+}
+
+export const addEmployee: RequestHandler = async (req, res) => {
+    const {name, functionName, zipCode, birthDate, photoURI} = req.body;
+    console.log(req.body)
+    await query(`INSERT INTO functionaries(name, position, zip_code, birth_date, photo_uri) VALUE ("${name}",
+                                                                                                   "${functionName}",
+                                                                                                   "${zipCode}",
+                                                                                                   "${birthDate}",
+                                                                                                   "${photoURI}")` as any);
+
+    res.send(200);
+}
+
+export const registerEmployeeCall: RequestHandler = async (req, res) => {
+    const {markedEmployees, date, constructionId, period} = req.body;
+
+    console.log(req.body)
+    for (const user of markedEmployees) {
+        await query(`insert into calls (date, constructionId, employeeId, periodo) value ("${date}", "${constructionId}", "${user.id}", "${period}")` as any)
+    }
+    ;
+}
