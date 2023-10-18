35374081eee9a86e7defa94fec5459881bd5a57f
LBruner <leandrobruner15@gmail.com>
2023-10-18 08:27:30 -0300

    add react router and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./components/UI/Navbar";
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import EmployeeRegister from "./pages/EmployeeRegister";
+import Teams from "./pages/Teams";
+import Employees from "./pages/Employees";
+import AddEmployee from "./pages/AddEmployee";
+import Constructions from "./pages/Constructions";
+import AddConstructions from "./pages/AddConstructions";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>OLA</h1>
-      </header>
-    </div>
-  );
+
+    return (
+        <>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/employeeRegister" element={<EmployeeRegister/>}/>
+                <Route path="/teams" element={<Teams/>}/>
+                <Route path="/employees" element={<Employees/>}/>
+                <Route path="/addEmployee" element={<AddEmployee/>}/>
+                <Route path="/constructions" element={<Constructions/>}/>
+                <Route path="/addConstruction" element={<AddConstructions/>}/>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
